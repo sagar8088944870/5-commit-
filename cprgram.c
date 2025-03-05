72a6e25b549826edca804a077fd337acce285f76
sagar8088944870 <sagar.m@s.amity.edu>
2025-03-05 13:56:32 +0530

    i added switch case statements

--- cprgram.c
+++ cprgram.c
@@ -25,3 +25,27 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("Enter your choice (1-5): ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("You chose option 1\n");
+            break;
+        case 2:
+            printf("You chose option 2\n");
+            break;
+        case 3:
+            printf("You chose option 3\n");
+            break;
+        case 4:
+            printf("You chose option 4\n");
+            break;
+        case 5:
+            printf("You chose option 5\n");
+            break
