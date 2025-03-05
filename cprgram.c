9dcddfcef34fc4895040aa0e96eeb227a984ecca
sagar8088944870 <sagar.m@s.amity.edu>
2025-03-05 14:00:10 +0530

    i added do while loop statements

--- cprgram.c
+++ cprgram.c
@@ -64,3 +64,15 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 5);
+
+    return 0;
+}
